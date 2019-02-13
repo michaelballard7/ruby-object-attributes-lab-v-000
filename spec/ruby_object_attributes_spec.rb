6362a8c948f8ac1a737f250d900e3c8352e659f6
Michael Ballard <ballard.michael9@gmail.com>
2019-02-13 21:01:36 +0000

    Automatically backed up by Learn

--- spec/ruby_object_attributes_spec.rb
+++ spec/ruby_object_attributes_spec.rb
@@ -6,14 +6,14 @@ describe 'Dog' do
     expect(Dog).to be_a(Class)
   end
 
-    describe '#name=' do
-      it 'writes the name of the dog to an instance variable @name' do
-        fido = Dog.new
-        fido.name = "Fido"
+  describe '#name=' do
+    it 'writes the name of the dog to an instance variable @name' do
+      fido = Dog.new
+      fido.name = "Fido"
 
-        expect(fido.instance_variable_get(:@name)).to eq("Fido")
-      end
+      expect(fido.instance_variable_get(:@name)).to eq("Fido")
     end
+  
 
     describe '#name' do
       it 'reads the name of the dog from an instance variable @name' do
