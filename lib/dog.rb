d8a9ffac3b287144b0f87d19a54e9760c7840d6b
Michael Ballard <ballard.michael9@gmail.com>
2019-02-13 21:03:53 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,10 +1,10 @@
 
 class Dog
 
-  def initialize(name, breed)
-    @name = name
-    @breed = breed
-  end
+  # def initialize(name, breed)
+  #   @name = name
+  #   @breed = breed
+  # end
 
   def name
     @name
